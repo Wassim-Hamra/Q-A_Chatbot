f98e98f8b374789df1866b2846a238cb3c7bfb1f
wassim <amriyassine195@gmail.com>
2024-12-05 00:22:43 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# **Sierra: Q&A Chatbot using LLMs**
+
+### This project is a Q&A chatbot built with large language models (LLMs) like Gemma2, Llama3.1, and Mixtral.
+
+
+### ***Technologies Used:***
+
+#### ***LangChain for integrating various language models and output parsers.***
+
+#### ***Groq for LLM-based interaction.***
+
+#### ***Streamlit for the web interface***
+  *  ***Web App link: [next-word-wassim-hamra.streamlit.app](https://next-word-prediction-wassim-hamraapp.streamlit.app/)***
+  * **⚠️ Wake the Streamlit Application if it's sleeping**
+
+### ***Setup:***
+
+#### **1- Clone this repository**
+
+#### **2- Install dependencies using pip install -r requirements.txt.**
+
+#### **3- Create a .env file for your API keys and model configurations.**
+
+#### **4- Run the app with streamlit run app.py.**
---
+++ streamlit_app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
+
+# Loading environ variables
+load_dotenv()
+
+# prompt template
+prompt = ChatPromptTemplate([
+    ('user','You are a helpful Q&A chatbot, your name is Sierra and you are created by Wassim Hamra. Answer the user questions in 4 sentences only'),
+    ('user','Question:{question}')
+]
+)
+
+def generate_response(question,model='gemma2-9b-it',temperature=1,max_tokens=200):
+    llm = ChatGroq(model_name=model, temperature=temperature, max_tokens=max_tokens)
+    parser = StrOutputParser()
+    chain = prompt|llm|parser
+    answer = chain.invoke({'question':question})
+    return answer
+
+# App
+# Sidebar
+st.title('Sierra : Q&A Chatbot')
+st.sidebar.subheader('Choose your parameters')
+model = st.sidebar.selectbox('Select A Model', ['Gemma2 9b','Llama3.1 8b','Mixtral8 7b'])
+if model == 'Gemma2 9b':
+    model = 'gemma2-9b-it'
+elif model == 'Llama3.1 8b':
+    model = 'llama-3.1-8b-instant'
+else:
+    model = 'mixtral-8x7b-32768'
+
+temperature = st.sidebar.slider('Temperature', min_value=float(0), max_value=float(2), value=float(1), step=0.1)
+st.sidebar.subheader('As the temperature approaches zero, the model will become deterministic and repetitive.')
+max_tokens = st.sidebar.slider('Maximum number of tokens', min_value=100, max_value=300, value=300)
+st.sidebar.subheader('The maximum number of tokens to generate.')
+
+
+# Main interface
+st.write('Ask Questions to Sierra')
+question = st.text_input('You: ')
+if question:
+    response = generate_response(question, model, temperature, max_tokens)
+    st.write('Sierra: ', response)
+else:
+    st.write('')
